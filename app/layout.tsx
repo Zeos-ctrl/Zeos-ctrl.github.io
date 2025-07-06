3ddc296ecda43d6005dd17f40158410997de3df4
Connor Bryan <astrilcodex@gmail.com>
2025-07-06 16:53:42 +0100

    updated description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Connor Portfolio',
+  description: 'Hi, I\'m Connor Bryan. I used to be a swimmer for Great Britain and now I’m studying for a Master’s degree in Data Intensive Physics. Feel free to check out my portfolio to see my previous projects, work experience, and recent achievements. These days, I’m especially interested in studying gravitational waves and how they help us understand the universe!',
 }
 
 export default function RootLayout({
